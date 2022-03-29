88f3bab65f576dfe39ac7a612b53d2b4be9bc778
TennilleW <89853068+TennilleW@users.noreply.github.com>
2022-03-29 12:26:07 -0400

    Day 2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Navbar from "./components/navbar.js";
 import Splash from "./components/splash.js";
-import Photos from "./components/photos.js";
 import Descriptions from "./components/descriptions.js";
 
 import "./App.css";
@@ -11,7 +10,6 @@ function App() {
     <div className="App">
       <Navbar />
       <Splash />
-      <Photos />
       <Descriptions />
     </div>
   );
